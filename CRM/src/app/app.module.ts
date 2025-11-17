26eb71283760e140c6ff65ae3542a4f041a6c7a1
Ssergmate <sergiomatesanzadrados@gmail.com>
2025-11-17 12:44:05 +0100

    Initial project setup (Angular 20, Core/Shared modules, HttpClient)

---
+++ CRM/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+
+import { CoreModule } from './core/core.module'; 
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+  ],
+  providers: [ 
+  provideHttpClient(withInterceptorsFromDi())
+  ],
+})
+export class AppModule { }
