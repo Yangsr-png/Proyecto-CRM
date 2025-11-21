8182745a28c94bc7a12b6f9d11c673826991a4e8
Ssergmate <sergiomatesanzadrados@gmail.com>
2025-11-21 13:45:14 +0100

    Edicion contacto/cliente

--- CRM/src/app/features/dashboard/dashboard.module.ts
+++ CRM/src/app/features/dashboard/dashboard.module.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { DashboardRoutingModule } from './dashboard.routing.module'; 
 import { DashboardHomeComponent } from './pages/dashboard.home/dashboard.home';
+import { ClientesTableComponent } from '../../components/clientes-table/clientes-table.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,10 @@ import { DashboardHomeComponent } from './pages/dashboard.home/dashboard.home';
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // 2. IMPORTANTE: Aquí va la Tabla (porque standalone: true)
+    // ¡Si lo pones en 'declarations' fallará! Tiene que ir en 'imports'.
+    ClientesTableComponent 
   ]
 })
 export class DashboardModule { }
