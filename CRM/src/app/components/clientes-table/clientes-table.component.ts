0c7a4212e2ad7cc45ce8d085d18069e17cba42f1
Yangsr-png <JeanIsr05@gmail.com>
2025-11-24 10:24:00 +0100

    Errores de merge arreglados

--- CRM/src/app/components/clientes-table/clientes-table.component.ts
+++ CRM/src/app/components/clientes-table/clientes-table.component.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { ClientesService } from '../../services/clientes.service';
 import { Cliente } from '../../models/cliente.model';
+import { ContactoFormComponent } from "../contacto-form/contacto-form.component";
 
 @Component({
   selector: 'app-clientes-table',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ContactoFormComponent],
   templateUrl: './clientes-table.component.html',
   styleUrls: ['./clientes-table.component.css']
 })
